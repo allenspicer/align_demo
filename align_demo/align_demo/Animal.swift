3bcd5cfe6f0d2a0123fc2a20e9ec8c81dce08b06
Allen Spicer - uBack <allenspicer@gmail.com>
2018-08-01 12:28:51 -0400

    Data model - struct

---
+++ align_demo/align_demo/Animal.swift
@@ -0,0 +1,17 @@
+//
+//  Animal.swift
+//  align_demo
+//
+//  Created by Allen Spicer on 8/1/18.
+//  Copyright © 2018 surfbreak. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Animal : Decodable{
+    var type = String()
+    var size = String()
+    var color = String()
+}
+
